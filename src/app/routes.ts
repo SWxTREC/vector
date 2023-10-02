7c72f9984ab4b47c14428ec62cccb13f5d8c6354
jknuth <Jennifer.Knuth@lasp.colorado.edu>
2023-10-02 13:54:23 -0600

    remove about page, point to new documentation

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -3,13 +3,10 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     {
         path: '',
-        loadChildren: () => import( './containers/about/about.module').then( m => m.AboutModule )
+        loadChildren: () => import( './containers/calculator/calculator.module').then( m => m.CalculatorModule )
     }, {
         path: 'docs',
         loadChildren: () => import( './containers/docs/docs.module').then( m => m.DocsModule )
-    }, {
-        path: 'calculator',
-        loadChildren: () => import( './containers/calculator/calculator.module').then( m => m.CalculatorModule )
     }, {
         path: '**',
         redirectTo: ''
